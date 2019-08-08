755b5318f5b6cec0aceb032a236550d5eba6b691
naresh <rnaresh.n@gmail.com>
2019-08-08 12:12:58 -0700

    theme support added :boom: :boom:

--- home.py
+++ home.py
@@ -1,6 +1,6 @@
 import json
 from flask import Blueprint, render_template, jsonify, request, current_app
-from flask_login import login_required, current_user
+from flask_login import current_user
 
 import db
 from utils import database
