56cc528662e6709ab43943f08097974bc32ca9df
naresh <rnaresh.n@gmail.com>
2019-08-08 20:27:36 -0700

    mobile overlapping cards fixed :hammer:

--- static/js/index.js
+++ static/js/index.js
@@ -3,7 +3,7 @@ let index = new Vue({
     vuetify: new Vuetify(),
     data: {
         current_page: "home",
-        drawer: null,
+        drawer: false,
         theme: {},
         projects: [],
         blogs: [],
