17e413cea20703d35624194f326b4bac23b13ada
Nicholas <kilia@pop-os.localdomain>
2019-08-30 13:37:29 +0700

    auto fetching medium article

---
+++ utils/medium.py
@@ -0,0 +1,57 @@
+import requests
+import json
+import logging
+from datetime import datetime
+
+
+def update_articles(db_conn, medium_url):
+    c = db_conn.cursor()
+    c.execute("DELETE FROM blogs")  # Clear all entries
+    try:
+        retrieving_posts(medium_url, db_conn)
+        db_conn.commit()
+    except Exception as e:
+        logging.warning(f"[E0003] {e}")
+
+
+def retrieving_posts(medium_url, db_conn):
+    api = medium_url + "/latest?format=json"
+    res = requests.get(api)
+    if not res:
+        raise Exception(
+            '''[E0001] An error occured when crawling data by page''')
+
+    content = json.loads(res.text.split('])}while(1);</x>')[1])
+    posts = content['payload']['references']['Post']
+    c = db_conn.cursor()
+    for idx, post in enumerate(posts):
+        post = posts[post]
+        title = post["title"]
+        description = post["content"]["subtitle"]
+        url = medium_url + "/" + post["uniqueSlug"]
+
+        previewImage = post["virtuals"]["previewImage"]
+        if previewImage["imageId"]:
+            image_url = "https://miro.medium.com/fit/c/%s/%s/%s" % (
+                previewImage["originalWidth"],
+                previewImage["originalHeight"],
+                previewImage["imageId"])
+        else:
+            image_url = "/static/img/personal-website-logo.webp"
+
+        try:
+            time_stamp = datetime.fromtimestamp(int(post["createdAt"] / 1000))
+
+        except Exception as e:
+            time_stamp = ""
+            logging.warning(f"[E0006] {e}")
+
+        try:
+            c.execute(
+                f"INSERT INTO blogs "
+                "(title, description, url, image_url, timestamp) "
+                "VALUES (?, ?, ?, ?, ?)",
+                (title, description, url, image_url, time_stamp))
+            db_conn.commit()
+        except Exception as e:
+            logging.warning(f"[E0004] {e}")
