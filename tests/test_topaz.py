e87d2517708e72580d416ed173f2ef7649edc5c2
naresh <rnaresh.n@gmail.com>
2019-11-03 11:19:11 -0800

    simple tests added

---
+++ tests/test_topaz.py
@@ -0,0 +1,32 @@
+import pytest
+
+from __init__ import create_app
+
+
+@pytest.fixture
+def client():
+    app = create_app()
+    app.config["TESTING"] = True
+
+    with app.test_client() as client:
+        yield client
+
+
+def test_home(client):
+    rv = client.get("/")
+    assert rv._status_code == 200, "Failed to load home page"
+
+
+def test_public_repos(client):
+    rv = client.get("/public_repos")
+    assert rv._status_code == 200, "Failed to load projects"
+
+
+def test_blogs(client):
+    rv = client.get("/blogs")
+    assert rv._status_code == 200, "Failed to load blogs"
+
+
+def test_publications(client):
+    rv = client.get("/publications")
+    assert rv._status_code == 200, "Failed to load publications"
