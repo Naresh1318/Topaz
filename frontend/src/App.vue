f450dd437c15e7aaad4430625cbaa2e6ed31454c
naresh1318 <rnaresh.n@gmail.com>
2020-05-07 00:44:05 -0700

    vuetify added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,15 @@
 <template>
-  <div id="app">
+  <v-app>
 <!--      <router-link to="/">Home</router-link>-->
 <!--      <router-link to="/projects">Projects</router-link>-->
 <!--      <router-link to="/blog">Blog</router-link>-->
 <!--      <router-link to="/about">About</router-link>-->
-    <a href="/blog">Test</a>
     <router-view/>
-  </div>
+  </v-app>
 </template>
 
-<style>
+<script>
+</script>
 
+<style>
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="home">
     <p>{{ page_name }}</p>
+    <nav-bar text="Calling from home"></nav-bar>
   </div>
 </template>
 
 <script>
+import NavBar from '../components/NavBar.vue';
+
 export default {
   name: 'Home',
   data() {
     return {
       page_name: 'Home',
     };
+  },
+  components: {
+    navBar: NavBar,
   },
 };
 </script>
