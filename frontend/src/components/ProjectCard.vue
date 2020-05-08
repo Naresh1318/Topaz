41001e092e9d6b8dc42504a3cec88ce66d974e07
naresh1318 <rnaresh.n@gmail.com>
2020-05-08 01:41:00 -0700

    init project card

---
+++ frontend/src/components/ProjectCard.vue
@@ -0,0 +1,94 @@
+<template>
+  <v-container style="position: relative">
+    <v-row>
+      <v-col cols="8">
+        <v-row>
+          <v-col>
+            <a :href="url">{{ title }}</a>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <p class="hide-overflow">{{ description }}</p>
+          </v-col>
+        </v-row>
+        <v-row style="position: absolute; bottom: 5px">
+          <v-col cols="6">
+            <v-row no-gutters>
+              <v-col>
+                <v-row no-gutters>
+                  <v-col>
+                    <span class="dot"
+                          :style="{'background-color': primary_language_color}">
+                    </span>
+                  </v-col>
+                  <v-col>
+                    {{primary_language}}
+                  </v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col cols="6">
+            <v-row no-gutters>
+              <v-col>
+                <v-row no-gutters>
+                  <v-col cols="6">
+                    <v-icon>fa-star</v-icon>
+                  </v-col>
+                  <v-col cols="6">
+                    {{ stars }}
+                  </v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="4">
+        <v-img height="200px" :src="image_url" contain></v-img>
+      </v-col>
+    </v-row>
+    <v-divider></v-divider>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'ProjectCard',
+  props: ['title', 'url', 'description', 'primary_language', 'primary_language_color', 'stars', 'image_url'],
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style scoped>
+a {
+  position: relative;
+  display: inline-block;
+  font-size: 24px;
+  font-weight: 500;
+  text-align: center;
+  color: #000 !important;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+.hide-overflow {
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+}
+
+.dot {
+  height: 15px;
+  width: 15px;
+  border-radius: 50%;
+  display: inline-block;
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,22 +1,43 @@
 <template>
   <div class="home">
-    <p>{{ page_name }}</p>
     <nav-bar active_page="Home"></nav-bar>
+    <v-content>
+      <div class="pa-3">
+        <project-card :title="projects[0].title" :url="projects[0].url"
+        :description="projects[0].description" :stars="projects[0].stars"
+        :primary_language="projects[0].primary_language"
+        :primary_language_color="projects[0].primary_language_color"
+        :image_url="projects[0].image_url"></project-card>
+      </div>
+    </v-content>
   </div>
 </template>
 
 <script>
 import NavBar from '../components/NavBar.vue';
+import ProjectCard from '../components/ProjectCard.vue';
 
 export default {
   name: 'Home',
   data() {
     return {
       page_name: 'Home',
+      projects: [
+        {
+          title: 'Topaz',
+          url: 'https://github.com/Naresh1318/Topaz',
+          description: 'Test text goes here cause it\'s a test',
+          primary_language: 'lol',
+          primary_language_color: '#123456',
+          stars: 1000,
+          image_url: 'https://raw.githubusercontent.com/Naresh1318/Dopamine/master/resources/icons/icon.png',
+        },
+      ],
     };
   },
   components: {
     navBar: NavBar,
+    projectCard: ProjectCard,
   },
 };
 </script>
