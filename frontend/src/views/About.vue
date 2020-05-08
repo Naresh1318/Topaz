e51d1fc77dfd68a3c975e6bf7e9dbeda820ec6da
naresh1318 <rnaresh.n@gmail.com>
2020-05-07 23:54:29 -0700

    added navigation bar to pages

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="about">
     <p>{{ page_name }}</p>
+    <nav-bar active_page="About"></nav-bar>
   </div>
 </template>
 
 <script>
+import NavBar from '../components/NavBar.vue';
+
 export default {
   name: 'About',
   data() {
     return {
       page_name: 'About',
     };
+  },
+  components: {
+    navBar: NavBar,
   },
 };
 </script>
--- frontend/src/views/Blog.vue
+++ frontend/src/views/Blog.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="blog">
     <p>{{ page_name }}</p>
+    <nav-bar active_page="Blog"></nav-bar>
   </div>
 </template>
 
 <script>
+import NavBar from '../components/NavBar.vue';
+
 export default {
   name: 'Blog',
   data() {
     return {
       page_name: 'Blog',
     };
+  },
+  components: {
+    navBar: NavBar,
   },
 };
 </script>
