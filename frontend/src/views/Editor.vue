4c639033b55ae7ae1c16109493685c6914dd5d8c
naresh1318 <rnaresh.n@gmail.com>
2020-06-02 23:48:15 -0700

    added way to edit content

--- frontend/src/views/Editor.vue
+++ frontend/src/views/Editor.vue
@@ -1,8 +1,27 @@
 <template>
   <div id="editor">
-    <h1>Editor</h1>
-    <textarea :value="markdown_content" @input="update"></textarea>
-    <div v-html="render_markdown"></div>
+    <v-content>
+      <v-container>
+        <v-progress-linear :active="loading" :indeterminate="loading"
+                           absolute top color="black accent-4">
+        </v-progress-linear>
+        <v-row>
+          <v-col>
+            <h1>Editor</h1>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col md="6" xs="12">
+            <textarea :value="markdown_content" @input="update"
+                      style="width: 100%; height: 100%">
+            </textarea>
+          </v-col>
+          <v-col md="6" xs="12">
+            <div v-html="render_markdown" class="content_html"></div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
   </div>
 </template>
 
@@ -19,6 +38,7 @@ export default {
       converter: new showdown.Converter({
         extensions: [showdownHighlight],
       }),
+      loading: true,
     };
   },
   computed: {
@@ -32,10 +52,43 @@ export default {
     update: loadsh.debounce(function (e) {
       this.markdown_content = e.target.value;
     }, 300),
+    get_markdown(page) {
+      this.$http.get(`${this.$backend_address}/markdown_content`, {
+        params: {
+          path: page,
+        },
+      })
+        .then((response) => {
+          this.loading = false;
+          this.markdown_content = response.data.markdown;
+        });
+    },
+  },
+  created() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    if (urlParams.has('page')) {
+      this.get_markdown(urlParams.get('page'));
+    } else {
+      this.markdown_content = '';
+    }
   },
 };
 </script>
 
 <style scoped>
+.content_html a {
+  text-decoration: underline;
+  color: black;
+}
+
+#editor textarea {
+  border: none;
+  resize: none;
+  outline: none;
+  background-color: #f6f6f6;
+  font-size: 16px;
+  padding: 20px;
+}
 
 </style>
