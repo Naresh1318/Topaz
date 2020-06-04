a5139ac90a809b64aa403a18302fdc95ca4e8071
naresh1318 <rnaresh.n@gmail.com>
2020-06-03 23:27:26 -0700

    version control based local doc management added

--- home.py
+++ home.py
@@ -1,4 +1,6 @@
+import datetime
 import json
+import subprocess
 
 from flask import Blueprint, jsonify, request, current_app
 from flask_login import current_user
@@ -92,7 +94,7 @@ def blogs():
         database.add_entry("blogs", db_conn, title, description, url, image_url, time_stamp)
         db_conn.close()
         return jsonify({"INFO": "Blog added"})
-    return jsonify({"ERROR": "Unauthenticated"})
+    return jsonify({"ERROR": "Unauthenticated"}), 401
 
 
 @bp.route("/publications", methods=["GET", "POST"])
@@ -118,14 +120,39 @@ def publications():
         database.add_entry("publications", db_conn, title, description, url, image_url, time_stamp)
         db_conn.close()
         return jsonify({"INFO": "Publication added"})
-    return jsonify({"ERROR": "Unauthenticated"})
+    return jsonify({"ERROR": "Unauthenticated"}), 401
 
 
-@bp.route("/markdown_content", methods=["GET"])
+@bp.route("/markdown_content", methods=["GET", "POST"])
 def markdown_content():
+    document_path = request.args.get("path")
+    if ".." in document_path or "~" in document_path:
+        return jsonify({"INFO": "Invalid path"}), 550
+    local_path = f"{current_app.config['MARKDOWN_DIR']}/{document_path}"
+
+    try:
+        subprocess.run(["git", "checkout", "local"], check=True)
+    except subprocess.CalledProcessError:
+        subprocess.run(["git", "checkout", "-b", "local"], check=True)
+
     if request.method == "GET":
-        document_path = request.args.get("path")
-        if ".." in document_path or "~" in document_path:
-            return jsonify({"INFO": "Invalid path"}), 550
-        with open(f"{current_app.config['MARKDOWN_DIR']}/{document_path}", "r") as f:
+        with open(local_path, "r") as f:
+            subprocess.run(["git", "checkout", "-"])
             return jsonify({"INFO": "Document found", "markdown": f.read()})
+    elif current_user.is_authenticated:
+        content = request.json["markdown"]
+        with open(local_path, "w") as f:
+            f.write(content)
+
+        # Commit changes
+        try:
+            saved_time = datetime.datetime.now()
+            subprocess.run(["git", "add", local_path], check=True)
+            subprocess.run(["git", "commit", "-m", f"{document_path}: {saved_time}"], check=True)
+            subprocess.run(["git", "checkout", "-"])
+            return jsonify({"INFO": "Document written", "time": str(saved_time)})
+        except subprocess.CalledProcessError:
+            subprocess.run(["git", "checkout", "-"])
+            return jsonify({"INFO": "Document not written"})
+    else:
+        return jsonify({"ERROR": "Unauthenticated"}), 401
